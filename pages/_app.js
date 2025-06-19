fc684f9b49ebbdd9ac14eb036ffd66cc4f44bcf5
antoinelavo <eugenepark912@gmail.com>
2025-06-19 20:04:09 +0900

    changed font loading

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,20 @@ import '../styles/globals.css';
 import Header from '@/components/Header'
 import Head from 'next/head';
 import Footer from '@/components/Footer';
+import { Noto_Sans_KR } from "next/font/google";
+
+const notoSansKR = Noto_Sans_KR({
+    variable: "--fonts--noto-sans-kr",
+    subsets: ["latin"],
+})
+
 
 export default function App({ Component, pageProps }) {
   const showHeader = !Component.hideHeader;
 
   return (
     <>
+    <html lang="en" className={notoSansKR.className}>
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -17,6 +25,7 @@ export default function App({ Component, pageProps }) {
       <Header />
       <Component {...pageProps} />
       <Footer />
+    </html>
     </>
   );
 }

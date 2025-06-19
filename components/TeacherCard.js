830daeabea86b279b13c4625444e5e8f959b8a0a
antoinelavo <eugenepark912@gmail.com>
2025-06-19 15:23:40 +0900

    Update TeacherCard.js

--- components/TeacherCard.js
+++ components/TeacherCard.js
@@ -1,5 +1,7 @@
 // components/TeacherCard.js
 import Link from 'next/link'
+import Image from 'next/image'
+
 
 export default function TeacherCard({
   name,
@@ -15,10 +17,12 @@ export default function TeacherCard({
       {/* ← Avatar + name/school */}
       <div className="flex items-center flex-1 justify-between px-[0.75em] py-[2em] md:p-[2em] gap-[1em] md:gap-[2em]">
         <div className="relative sm:w-14 sm:h-14 w-10 h-10 overflow-hidden rounded-full flex-0">
-          <img
+          <Image
             src={profile_picture}
             alt={`${name} 프로필 사진`}
-            fill = "true"
+            fill
+            sizes="(max-width: 48px)"
+            priority={false}
             className="object-cover"
           />
         </div>
